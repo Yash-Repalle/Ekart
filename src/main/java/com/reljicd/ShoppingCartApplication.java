fcb071c0a2f81dc8a97e85291dd92b995818df61
Yaswanth345 <yaswanthgopi918@gmail.com>
2023-09-12 17:46:34 +0530

    jenkins file

--- src/main/java/com/reljicd/ShoppingCartApplication.java
+++ src/main/java/com/reljicd/ShoppingCartApplication.java
@@ -1,9 +1,11 @@
 package com.reljicd;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@EnableAutoConfiguration
 public class ShoppingCartApplication {
 
     public static void main(String[] args) {
